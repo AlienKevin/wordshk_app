67c22d0e8506c7fd87d466616dfc1da1e97de079
Kevin Li <kevinli020508@gmail.com>
2021-12-28 17:54:41 -0500

    Use Mutex for rust local states and fix rust logging

--- rust/src/api.rs
+++ rust/src/api.rs
@@ -9,6 +9,11 @@ use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io::prelude::*;
 use std::path::Path;
+use parking_lot::Mutex;
+use lazy_static::lazy_static;
+use anyhow::{anyhow, Result};
+use oslog::{OsLogger};
+use log::{LevelFilter, info};
 
 #[derive(Serialize, Deserialize)]
 struct Api {
@@ -33,6 +38,15 @@ fn deserialize_api<P: AsRef<Path>>(input_path: &P) -> Option<Api> {
 
 impl Api {
     pub fn new(app_dir: String) -> Self {
+        if !*IS_LOG_INITIALIZED.lock() {
+            OsLogger::new("hk.words")
+                .level_filter(LevelFilter::Debug)
+                .category_level_filter("Settings", LevelFilter::Trace)
+                .init()
+                .unwrap();
+            *IS_LOG_INITIALIZED.lock() = true;
+        }
+        info!("Calling Api::new()...");
         let api_path = Path::new(&app_dir).join("api.json");
         let current_time = Utc::now();
         match deserialize_api(&api_path) {
@@ -60,22 +74,28 @@ impl Api {
     }
 
     fn get_new_dict<P: AsRef<Path>>(api_path: &P) -> Api {
+        info!("Calling Api::get_new_dict()...");
         let new_release_time = Utc::now();
         let csv_url = "https://words.hk/static/all.csv.gz";
         let csv_gz_data = reqwest::blocking::get(csv_url).unwrap().bytes().unwrap();
+        info!("Downloaded CSV data");
         let mut gz = GzDecoder::new(&csv_gz_data[..]);
         let mut csv_data = String::new();
         gz.read_to_string(&mut csv_data).unwrap();
+        info!("Decompressed CSV data");
         let csv_data_remove_first_line = csv_data.get(csv_data.find('\n').unwrap() + 1..).unwrap();
         let csv_data_remove_two_lines = csv_data_remove_first_line
             .get(csv_data_remove_first_line.find('\n').unwrap() + 1..)
             .unwrap();
+        info!("Cleaned CSV data");
         let dict = parse_dict(csv_data_remove_two_lines.as_bytes()).unwrap();
+        info!("Parsed CSV data");
         let new_api = Api {
             dict: enrich_dict(&dict),
             release_time: new_release_time,
         };
         serialize_api(api_path, &new_api);
+        info!("Serialized api data");
         new_api
     }
 }
@@ -137,30 +157,23 @@ fn variant_search_helper(
     results
 }
 
-use std::cell::RefCell;
-use anyhow::{anyhow, Result};
-
-thread_local!(static API: RefCell<Option<Api>> = RefCell::new(None));
-thread_local!(static APP_DIR: RefCell<Option<String>> = RefCell::new(None));
+lazy_static! {
+    static ref API: Mutex<Option<Api>> = Mutex::new(None);
+    static ref APP_DIR: Mutex<Option<String>> = Mutex::new(None);
+    static ref IS_LOG_INITIALIZED: Mutex<bool> = Mutex::new(false);
+}
 
 pub fn init_api(input_app_dir: String) -> Result<()> {
-    APP_DIR.with(|app_dir_cell| {
-        *app_dir_cell.borrow_mut() = Some(input_app_dir.clone());
-    });
-    API.with(|api_cell| {
-        *api_cell.borrow_mut() = Some(Api::new(input_app_dir));
-    });
+    info!("Calling init_api()...");
+    *APP_DIR.lock() = Some(input_app_dir.clone());
+    *API.lock() = Some(Api::new(input_app_dir));
     Ok(())
 }
 
 pub fn pr_search(capacity: u32, query: String) -> Result<Vec<PrSearchResult>> {
-    API.with(|api_cell|
-        Ok(api_cell.borrow().as_ref().unwrap().pr_search(capacity, &query))
-    )
+    Ok((*API.lock()).as_ref().unwrap().pr_search(capacity, &query))
 }
 
 pub fn variant_search(capacity: u32, query: String) -> Result<Vec<VariantSearchResult>> {
-    API.with(|api_cell|
-        Ok(api_cell.borrow().as_ref().unwrap().variant_search(capacity, &query))
-    )
+    Ok((*API.lock()).as_ref().unwrap().variant_search(capacity, &query))
 }
