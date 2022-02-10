88fb83089610e296db3ea4f88d96012e1e40fa25
Kevin Li <kevinli020508@gmail.com>
2022-02-10 13:46:42 -0500

    Add constructors and extend Equatable

--- lib/entry.dart
+++ lib/entry.dart
@@ -1,4 +1,6 @@
-class Entry {
+import 'package:equatable/equatable.dart';
+
+class Entry extends Equatable {
   final int id;
   final List<Variant> variants;
   final List<String> poses;
@@ -6,6 +8,15 @@ class Entry {
   final List<String> sims;
   final List<String> ants;
   final List<Def> defs;
+
+  const Entry(
+      {required this.id,
+      required this.variants,
+      required this.poses,
+      required this.labels,
+      required this.sims,
+      required this.ants,
+      required this.defs});
 
   Entry.fromJson(Map<String, dynamic> json)
       : id = json['id'],
@@ -19,13 +30,23 @@ class Entry {
         defs = List.from(json['defs']).map((def) {
           return Def.fromJson(def);
         }).toList();
+
+  @override
+  List<Object> get props => [id, variants, poses, labels, sims, ants, defs];
 }
 
-class Def {
+class Def extends Equatable {
   final Clause yue;
   final Clause? eng;
   final List<AltClause> alts;
   final List<Eg> egs;
+
+  const Def({
+    required this.yue,
+    required this.eng,
+    required this.alts,
+    required this.egs,
+  });
 
   Def.fromJson(Map<String, dynamic> json)
       : yue = Clause.fromJson(json['yue']),
@@ -36,24 +57,37 @@ class Def {
         egs = List.from(json['egs']).map((eg) {
           return Eg.fromJson(eg);
         }).toList();
+
+  @override
+  List<Object?> get props => [yue, eng, alts, egs];
 }
 
-class Clause {
+class Clause extends Equatable {
   final List<Line> lines;
+
+  const Clause(this.lines);
 
   Clause.fromJson(List<String> json)
       : lines = List.from(json).map((line) {
           return Line.fromJson(line);
         }).toList();
+
+  @override
+  List<Object?> get props => [lines];
 }
 
-class Line {
+class Line extends Equatable {
   final List<Segment> segments;
+
+  const Line(this.segments);
 
   Line.fromJson(List<String> json)
       : segments = List.from(json).map((segment) {
           return Segment.fromJson(segment);
         }).toList();
+
+  @override
+  List<Object?> get props => [segments];
 }
 
 enum SegmentType {
@@ -65,13 +99,18 @@ SegmentType segmentTypeFromString(String string) {
   return string == 'Text' ? SegmentType.text : SegmentType.link;
 }
 
-class Segment {
+class Segment extends Equatable {
   final SegmentType type;
   final String segment;
+
+  const Segment(this.type, this.segment);
 
   Segment.fromJson(List<String> json)
       : type = segmentTypeFromString(json[0]),
         segment = json[1];
+
+  @override
+  List<Object?> get props => [type, segment];
 }
 
 enum RichLineType {
@@ -79,24 +118,34 @@ enum RichLineType {
   word,
 }
 
-class RichLine {
+class RichLine extends Equatable {
   final RichLineType type;
   final dynamic line;
+
+  const RichLine(this.type, this.line);
 
   RichLine.fromJson(Map<String, dynamic> json)
       : type = json['Ruby'] != null ? RichLineType.ruby : RichLineType.word,
         line = json['Ruby'] != null
             ? RubyLine.fromJson(json['Ruby'])
             : WordLine.fromJson(json['Word']);
+
+  @override
+  List<Object?> get props => [type, line];
 }
 
-class RubyLine {
+class RubyLine extends Equatable {
   final List<RubySegment> segments;
+
+  const RubyLine(this.segments);
 
   RubyLine.fromJson(List<dynamic> json)
       : segments = json.map((segment) {
           return RubySegment.fromJson(segment);
         }).toList();
+
+  @override
+  List<Object?> get props => [segments];
 }
 
 enum RubySegmentType {
@@ -105,9 +154,11 @@ enum RubySegmentType {
   linkedWord,
 }
 
-class RubySegment {
+class RubySegment extends Equatable {
   final RubySegmentType type;
   final dynamic segment;
+
+  const RubySegment(this.type, this.segment);
 
   RubySegment.fromJson(Map<String, dynamic> json)
       : type = json['Punc'] != null
@@ -119,51 +170,79 @@ class RubySegment {
             (json['Word'] != null
                 ? RubySegmentWord.fromJson(json['Word'])
                 : RubySegmentLinkedWord.fromJson(json['LinkedWord']));
+
+  @override
+  List<Object?> get props => [type, segment];
 }
 
-class RubySegmentWord {
+class RubySegmentWord extends Equatable {
   final Word word;
   final List<String> prs;
+
+  const RubySegmentWord(this.word, this.prs);
 
   RubySegmentWord.fromJson(List<dynamic> json)
       : word = Word.fromJson(json[0]),
         prs = List.from(json);
+
+  @override
+  List<Object?> get props => [word, prs];
 }
 
-class RubySegmentLinkedWord {
+class RubySegmentLinkedWord extends Equatable {
   final List<RubySegmentWord> words;
+
+  const RubySegmentLinkedWord(this.words);
 
   RubySegmentLinkedWord.fromJson(List<dynamic> json)
       : words = List.from(json).map((word) {
           return RubySegmentWord.fromJson(word);
         }).toList();
+
+  @override
+  List<Object?> get props => [words];
 }
 
-class WordLine {
+class WordLine extends Equatable {
   final List<WordSegment> segments;
+
+  const WordLine(this.segments);
 
   WordLine.fromJson(List<dynamic> json)
       : segments = List.from(json).map((word) {
           return WordSegment.fromJson(word);
         }).toList();
+
+  @override
+  List<Object?> get props => [segments];
 }
 
-class WordSegment {
+class WordSegment extends Equatable {
   final SegmentType type;
   final Word word;
+
+  const WordSegment(this.type, this.word);
 
   WordSegment.fromJson(List<dynamic> json)
       : type = segmentTypeFromString(json[0]),
         word = Word.fromJson(json[1]);
+
+  @override
+  List<Object?> get props => [type, word];
 }
 
-class Word {
+class Word extends Equatable {
   final List<Text> texts;
+
+  const Word(this.texts);
 
   Word.fromJson(List<dynamic> json)
       : texts = List.from(json).map((text) {
           return Text.fromJson(text);
         }).toList();
+
+  @override
+  List<Object?> get props => [texts];
 }
 
 enum TextStyle {
@@ -171,41 +250,65 @@ enum TextStyle {
   normal,
 }
 
-class Text {
+class Text extends Equatable {
   final TextStyle style;
   final String text;
+
+  const Text(this.style, this.text);
 
   Text.fromJson(List<String> json)
       : style = json[0] == 'Bold' ? TextStyle.bold : TextStyle.normal,
         text = json[1];
+
+  @override
+  List<Object?> get props => [style, text];
 }
 
-class Eg {
+class Eg extends Equatable {
   final RichLine? zho;
   final RichLine? yue;
   final Line? eng;
+
+  const Eg({
+    required this.zho,
+    required this.yue,
+    required this.eng,
+  });
 
   Eg.fromJson(Map<String, dynamic> json)
       : zho = json['zho'] == null ? null : RichLine.fromJson(json['zho']),
         yue = json['yue'] == null ? null : RichLine.fromJson(json['yue']),
         eng = json['eng'] == null ? null : Line.fromJson(json['eng']);
+
+  @override
+  List<Object?> get props => [zho, yue, eng];
 }
 
-class AltClause {
+class AltClause extends Equatable {
   // TODO: Change to enum
   final String altLang;
   final Clause clause;
 
+  const AltClause(this.altLang, this.clause);
+
   AltClause.fromJson(List<dynamic> json)
       : altLang = json[0],
         clause = Clause.fromJson(json[1]);
+
+  @override
+  List<Object?> get props => [altLang, clause];
 }
 
-class Variant {
+class Variant extends Equatable {
   final String word;
   final String prs;
+
+  const Variant(this.word, this.prs);
 
   Variant.fromJson(Map<String, dynamic> json)
       : word = json['word'],
         prs = json['prs'];
+
+  @override
+  List<Object?> get props => [word, prs];
 }
