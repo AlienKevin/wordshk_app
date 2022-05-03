0f00c8c6934687ac53d35a771d1bc56dd8acb4ef
Kevin Li <kevinli020508@gmail.com>
2022-05-03 14:06:23 -0400

    Rename app to words.hk, add icon and splash screen

---
+++ generate_app_name.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+flutter pub get
+flutter pub run flutter_launcher_name:main
---
+++ generate_icons.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pngquant assets/icon_512.png --force --quality 65-80 --output assets/icon_512.png
+
+### Clean and rebuild flutter ###
+flutter clean
+flutter pub get
+
+### Generate iOS and Android icons ###
+flutter pub run flutter_launcher_icons:main -f pubspec.yaml
+
+#### Generate splash screen for Web, Android, and iOS ###
+flutter pub run flutter_native_splash:create
