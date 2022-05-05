7a9ac4de83e5ca6ea9deacfa02cde198c9de0c1c
Kevin Li <kevinli020508@gmail.com>
2022-05-05 09:19:57 -0400

    Make greyColor darker

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 const blueColor = Color(0xff4ea0ea);
 const whiteColor = Color(0xffffffff);
 const lightGreyColor = Color(0xffDEE2E6);
-const greyColor = Color(0xffacadb2);
+const greyColor = Color(0xff6C757D);
 const blackColor = Color(0xff000000);
 const double appBarHeight = 60.0;
 const jyutpingFemaleSyllableNames = {
