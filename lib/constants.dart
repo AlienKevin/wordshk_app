8bb16ab4d43eeaf44acde0a47f4c238b1e19951c
Kevin Li <kevinli020508@gmail.com>
2022-05-07 12:18:40 -0400

    Make blueColor darker

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-const blueColor = Color(0xff4ea0ea);
+const blueColor = Color(0xff0B83D0);
 const whiteColor = Color(0xffffffff);
 const lightGreyColor = Color(0xffDEE2E6);
 const greyColor = Color(0xff6C757D);
