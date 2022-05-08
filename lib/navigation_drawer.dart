b72604241f288e46586455f7e7ec1bc30ba82e69
Kevin Li <kevinli020508@gmail.com>
2022-05-08 10:21:13 -0400

    Extract drawerButton() and unbold drawerButtons

--- lib/navigation_drawer.dart
+++ lib/navigation_drawer.dart
@@ -9,82 +9,76 @@ class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => SizedBox(
-      width: 250,
-      child: Drawer(
+  Widget build(BuildContext context) {
+    TextButton drawerButton(String label, IconData icon, gotoPage) =>
+        TextButton.icon(
+          icon: Padding(
+              padding: const EdgeInsets.only(left: 10), child: Icon(icon)),
+          label: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                label,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleSmall
+                    ?.copyWith(fontWeight: FontWeight.normal),
+              )),
+          onPressed: () {
+            Navigator.of(context).pop();
+            Navigator.push(
+              context,
+              CustomPageRoute(builder: gotoPage),
+            );
+          },
+        );
+
+    return SizedBox(
+        width: 250,
+        child: Drawer(
 // Add a ListView to the drawer. This ensures the user can scroll
 // through the options in the drawer if there isn't enough vertical
 // space to fit everything.
-        child: ListView(
+          child: ListView(
 // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            SizedBox(
-              height: 210,
-              child: DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
+            padding: EdgeInsets.zero,
+            children: [
+              SizedBox(
+                height: 210,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "words.hk",
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge
+                              ?.copyWith(color: whiteColor),
+                        ),
+                        Text(
+                          'Crowd-sourced Cantonese dictionary for everyone.',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall
+                              ?.copyWith(color: whiteColor),
+                        )
+                      ]),
                 ),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "words.hk",
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleLarge
-                            ?.copyWith(color: whiteColor),
-                      ),
-                      Text(
-                        'Crowd-sourced Cantonese dictionary for everyone.',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodySmall
-                            ?.copyWith(color: whiteColor),
-                      )
-                    ]),
               ),
-            ),
-            TextButton.icon(
-              icon: const Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: Icon(Icons.search)),
-              label: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Dictionary',
-                    style: Theme.of(context).textTheme.titleSmall,
-                  )),
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  CustomPageRoute(
-                      builder: (context) => const HomePage(
-                            title: 'words.hk',
-                          )),
-                );
-              },
-            ),
-            const Divider(),
-            TextButton.icon(
-              icon: const Padding(
-                  padding: EdgeInsets.only(left: 10), child: Icon(Icons.info)),
-              label: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'About words.hk',
-                    style: Theme.of(context).textTheme.titleSmall,
-                  )),
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  CustomPageRoute(builder: (context) => const AboutPage()),
-                );
-              },
-            ),
-          ],
-        ),
-      ));
+              drawerButton(
+                  "Dictionary",
+                  Icons.search,
+                  (_) => const HomePage(
+                        title: 'words.hk',
+                      )),
+              const Divider(),
+              drawerButton(
+                  "About words.hk", Icons.info, (_) => const AboutPage()),
+            ],
+          ),
+        ));
+  }
 }
