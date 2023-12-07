93ae306b9a35432a9a12b111f3648813ff704acc
Kevin Li <kevinli020508@gmail.com>
2023-12-07 11:31:32 -0500

    Center and constrain the size of introduction_page.dart on wide screens

--- lib/pages/introduction_page.dart
+++ lib/pages/introduction_page.dart
@@ -5,6 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wordshk/pages/home_page.dart';
 import 'package:wordshk/widgets/preferences/title.dart';
 
+import '../constants.dart';
 import '../custom_page_route.dart';
 import '../widgets/preferences/language_radio_list_tiles.dart';
 import '../widgets/preferences/romanization_radio_list_tiles.dart';
@@ -22,58 +23,75 @@ class IntroductionPage extends StatelessWidget {
       titlePadding: EdgeInsets.only(top: 0.0, bottom: 24.0),
       contentMargin: EdgeInsets.all(32.0),
     );
-    return IntroductionScreen(
-        safeAreaList: const [false, false, true, true],
-        onDone: () {
-          Navigator.push(
-            context,
-            CustomPageRoute(
-                builder: (context) => const HomePage(title: "words.hk")),
-          );
-          prefs.setBool("firstTimeUser", false);
-        },
-        showSkipButton: false,
-        skipOrBackFlex: 0,
-        nextFlex: 0,
-        showBackButton: true,
-        //rtl: true, // Display as right-to-left
-        back: const Icon(Icons.arrow_back),
-        next: const Icon(Icons.arrow_forward),
-        done: Text(s.done, style: const TextStyle(fontWeight: FontWeight.w600)),
-        pages: [
-          PageViewModel(
-            titleWidget: Align(
-                alignment: Alignment.centerLeft,
-                child: PreferencesTitle(title: s.welcome)),
-            bodyWidget: Column(children: [
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(s.introductionText)),
-              const SizedBox(height: 40),
-              Image(
-                  width: 200,
-                  image: MediaQuery.of(context).platformBrightness ==
-                          Brightness.light
-                      ? const AssetImage('assets/icon.png')
-                      : const AssetImage('assets/icon_grey.png'))
-            ]),
-            decoration: pageDecoration,
+    return Container(
+      decoration: BoxDecoration(color: Theme.of(context).canvasColor.withOpacity(0.9)),
+      child: Align(
+        alignment: Alignment.center,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: wideScreenThreshold,
+            maxHeight: wideScreenThreshold,
           ),
-          PageViewModel(
-            titleWidget: Align(
-                alignment: Alignment.centerLeft,
-                child: PreferencesTitle(title: s.introductionLanguage)),
-            bodyWidget: const LanguageRadioListTiles(),
-            decoration: pageDecoration,
-          ),
-          PageViewModel(
-            titleWidget: Align(
-                alignment: Alignment.centerLeft,
-                child: PreferencesTitle(title: s.introductionRomanization)),
-            bodyWidget: const RomanizationRadioListTiles(
-                syncEntryRomanization: true),
-            decoration: pageDecoration,
-          )
-        ]);
+          child: IntroductionScreen(
+              safeAreaList: const [false, false, true, true],
+              onDone: () {
+                Navigator.push(
+                  context,
+                  CustomPageRoute(
+                      builder: (context) =>
+                          const HomePage(title: "words.hk")),
+                );
+                prefs.setBool("firstTimeUser", false);
+              },
+              globalBackgroundColor: Theme.of(context).cardColor,
+              showSkipButton: false,
+              skipOrBackFlex: 0,
+              nextFlex: 0,
+              showBackButton: true,
+              //rtl: true, // Display as right-to-left
+              back: const Icon(Icons.arrow_back),
+              next: const Icon(Icons.arrow_forward),
+              done: Text(s.done,
+                  style: const TextStyle(fontWeight: FontWeight.w600)),
+              pages: [
+                PageViewModel(
+                  titleWidget: Align(
+                      alignment: Alignment.centerLeft,
+                      child: PreferencesTitle(title: s.welcome)),
+                  bodyWidget: Column(children: [
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(s.introductionText)),
+                    const SizedBox(height: 40),
+                    Image(
+                        width: 200,
+                        image: MediaQuery.of(context).platformBrightness ==
+                                Brightness.light
+                            ? const AssetImage('assets/icon.png')
+                            : const AssetImage('assets/icon_grey.png'))
+                  ]),
+                  decoration: pageDecoration,
+                ),
+                PageViewModel(
+                  titleWidget: Align(
+                      alignment: Alignment.centerLeft,
+                      child:
+                          PreferencesTitle(title: s.introductionLanguage)),
+                  bodyWidget: const LanguageRadioListTiles(),
+                  decoration: pageDecoration,
+                ),
+                PageViewModel(
+                  titleWidget: Align(
+                      alignment: Alignment.centerLeft,
+                      child: PreferencesTitle(
+                          title: s.introductionRomanization)),
+                  bodyWidget: const RomanizationRadioListTiles(
+                      syncEntryRomanization: true),
+                  decoration: pageDecoration,
+                )
+              ]),
+        ),
+      ),
+    );
   }
 }
