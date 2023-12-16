a1d860c00f49cce3619e82edf568f3db57165c61
Kevin Li <kevinli020508@gmail.com>
2023-12-16 18:54:26 -0500

    Add a page to enable/disable analytics in introduction_page.dart

--- lib/pages/introduction_page.dart
+++ lib/pages/introduction_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wordshk/pages/home_page.dart';
+import 'package:wordshk/states/analytics_settings_state.dart';
 import 'package:wordshk/widgets/preferences/title.dart';
 
 import '../constants.dart';
 import '../custom_page_route.dart';
 import '../widgets/preferences/language_radio_list_tiles.dart';
+import '../widgets/preferences/radio_list_tile.dart';
 import '../widgets/preferences/romanization_radio_list_tiles.dart';
 
 class IntroductionPage extends StatelessWidget {
@@ -24,7 +27,8 @@ class IntroductionPage extends StatelessWidget {
       contentMargin: EdgeInsets.all(32.0),
     );
     return Container(
-      decoration: BoxDecoration(color: Theme.of(context).canvasColor.withOpacity(0.9)),
+      decoration:
+          BoxDecoration(color: Theme.of(context).canvasColor.withOpacity(0.9)),
       child: Align(
         alignment: Alignment.center,
         child: ConstrainedBox(
@@ -38,8 +42,7 @@ class IntroductionPage extends StatelessWidget {
                 Navigator.push(
                   context,
                   CustomPageRoute(
-                      builder: (context) =>
-                          const HomePage(title: "words.hk")),
+                      builder: (context) => const HomePage(title: "words.hk")),
                 );
                 prefs.setBool("firstTimeUser", false);
               },
@@ -75,23 +78,63 @@ class IntroductionPage extends StatelessWidget {
                 PageViewModel(
                   titleWidget: Align(
                       alignment: Alignment.centerLeft,
-                      child:
-                          PreferencesTitle(title: s.introductionLanguage)),
+                      child: PreferencesTitle(title: s.introductionLanguage)),
                   bodyWidget: const LanguageRadioListTiles(),
                   decoration: pageDecoration,
                 ),
                 PageViewModel(
                   titleWidget: Align(
                       alignment: Alignment.centerLeft,
-                      child: PreferencesTitle(
-                          title: s.introductionRomanization)),
+                      child:
+                          PreferencesTitle(title: s.introductionRomanization)),
                   bodyWidget: const RomanizationRadioListTiles(
                       syncEntryRomanization: true),
+                  decoration: pageDecoration,
+                ),
+                PageViewModel(
+                  titleWidget: Align(
+                      alignment: Alignment.centerLeft,
+                      child: PreferencesTitle(title: s.sendAnalytics)),
+                  bodyWidget: Column(children: [
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(s.sendAnalyticsDescription)),
+                    const SizedBox(height: 10),
+                    const AnalyticsRadioListTiles(),
+                  ]),
                   decoration: pageDecoration,
                 )
               ]),
         ),
       ),
     );
+  }
+}
+
+class AnalyticsRadioListTiles extends StatelessWidget {
+  const AnalyticsRadioListTiles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final enabled = context.watch<AnalyticsSettingsState>().enabled;
+
+    onEnabledChange(bool? newEnabled) {
+      if (newEnabled != null) {
+        context.read<AnalyticsSettingsState>().setEnabled(newEnabled);
+      }
+    }
+
+    analyticsRadioListTile(bool value) => PreferencesRadioListTile<bool>(
+        title: value
+            ? AppLocalizations.of(context)!.agreeToShare
+            : AppLocalizations.of(context)!.doNotShare,
+        value: value,
+        groupValue: enabled,
+        onChanged: onEnabledChange);
+
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      analyticsRadioListTile(true),
+      analyticsRadioListTile(false),
+    ]);
   }
 }
