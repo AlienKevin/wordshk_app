282752604a0270b550a04c5895a72a327d77a1c7
Kevin Li <kevinli020508@gmail.com>
2024-01-31 10:16:32 -0500

    Animate expandable arrow

--- lib/widgets/expandable.dart
+++ lib/widgets/expandable.dart
@@ -2,23 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-Widget expandButton(
-        String text, IconData icon, TextStyle lineTextStyle, onPressed) =>
-    SelectionContainer.disabled(
-      child: TextButton(
-          onPressed: onPressed,
-          style: TextButton.styleFrom(
-            padding: const EdgeInsets.all(0),
-            minimumSize: const Size(0, 0),
-          ),
-          child: Text.rich(TextSpan(
-            children: [
-              TextSpan(text: text, style: lineTextStyle),
-              WidgetSpan(child: Icon(icon, color: lineTextStyle.color!))
-            ],
-          ))),
-    );
-
 class MyExpandable extends StatefulWidget {
   final Widget child;
   final String collapseText;
@@ -27,46 +10,74 @@ class MyExpandable extends StatefulWidget {
 
   const MyExpandable(
       {super.key,
-      required this.child,
-      required this.collapseText,
-      required this.expandText,
-      required this.lineTextStyle});
+        required this.child,
+        required this.collapseText,
+        required this.expandText,
+        required this.lineTextStyle});
 
   @override
   State<MyExpandable> createState() => _MyExpandableState();
 }
 
-class _MyExpandableState extends State<MyExpandable> {
+class _MyExpandableState extends State<MyExpandable>
+    with SingleTickerProviderStateMixin {
   bool expanded = false;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300), // Adjust the duration as needed
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void toggleExpand() {
+    setState(() {
+      expanded = !expanded;
+      if (expanded) {
+        _controller.forward(); // Expand animation
+      } else {
+        _controller.reverse(); // Collapse animation
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return expanded
-        ? Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              widget.child,
-              expandButton(
-                  widget.collapseText,
-                  isMaterial(context)
-                      ? Icons.expand_less
-                      : CupertinoIcons.chevron_up,
-                  widget.lineTextStyle, () {
-                setState(() {
-                  expanded = false;
-                });
-              }),
-            ],
-          )
-        : expandButton(
-            widget.expandText,
-            isMaterial(context)
-                ? Icons.expand_more
-                : CupertinoIcons.chevron_down,
-            widget.lineTextStyle, () {
-            setState(() {
-              expanded = true;
-            });
-          });
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SelectionContainer.disabled(
+          child: GestureDetector(
+            onTap: toggleExpand,
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(text: expanded ? widget.collapseText : widget.expandText, style: widget.lineTextStyle),
+                  WidgetSpan(
+                    child: RotationTransition(
+                      turns: Tween<double>(begin: 0, end: 0.5).animate(_controller), // Rotate icon
+                      child: Icon(
+                        isMaterial(context) ? Icons.expand_more : CupertinoIcons.chevron_down,
+                        color: widget.lineTextStyle.color!,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        expanded ? widget.child : const SizedBox.shrink(),
+      ],
+    );
   }
 }
