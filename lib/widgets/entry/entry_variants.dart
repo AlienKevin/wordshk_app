61bc39730640c13311ed8cd65b898354cc185bc2
Kevin Li <kevinli020508@gmail.com>
2024-02-01 16:41:45 -0500

    Always show all variants

--- lib/widgets/entry/entry_variants.dart
+++ lib/widgets/entry/entry_variants.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:wordshk/src/rust/api/api.dart' show Script;
 
 import '../../models/entry.dart';
-import '../expandable.dart';
 import 'entry_variant.dart';
 
 class EntryVariants extends StatelessWidget {
@@ -32,36 +30,16 @@ class EntryVariants extends StatelessWidget {
           prTextStyle: prTextStyle,
         )
       : Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-          EntryVariant(
-            variant:
-                script == Script.simplified ? variantsSimp[0] : variants[0],
-            variantTextStyle: variantTextStyle,
-            prTextStyle: prTextStyle,
-          ),
-          MyExpandable(
-              lineTextStyle: lineTextStyle.copyWith(
-                  color: Theme.of(context).colorScheme.secondary),
-              expandText: AppLocalizations.of(context)!.entryMoreVariants,
-              collapseText: AppLocalizations.of(context)!.entryCollapseVariants,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: variants
-                            .asMap()
-                            .entries
-                            .skip(1)
-                            .map((variant) => EntryVariant(
-                                  variant: script == Script.simplified
-                                      ? variantsSimp[variant.key]
-                                      : variant.value,
-                                  variantTextStyle: variantTextStyle,
-                                  prTextStyle: prTextStyle,
-                                ))
-                            .toList()),
-                  ]))
-        ]);
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: variants
+              .asMap()
+              .entries
+              .map((variant) => EntryVariant(
+                    variant: script == Script.simplified
+                        ? variantsSimp[variant.key]
+                        : variant.value,
+                    variantTextStyle: variantTextStyle,
+                    prTextStyle: prTextStyle,
+                  ))
+              .toList());
 }
