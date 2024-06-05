451bdee5a04e5b4e506f3a857fdecea18f0e6d87
Kevin Li <kevinli020508@gmail.com>
2024-06-05 08:39:17 +0800

    Localize privacy policy page name

--- lib/pages/privacy_policy_page.dart
+++ lib/pages/privacy_policy_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:provider/provider.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:wordshk/states/language_state.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class PrivacyPolicyPage extends StatelessWidget {
   Future<String> _loadPolicyMarkdown(String lang) async {
@@ -16,7 +18,7 @@ class PrivacyPolicyPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Privacy Policy'),
+        title: Text(AppLocalizations.of(context)!.privacyPolicy),
       ),
       body: FutureBuilder(
         future: _loadPolicyMarkdown(lang),
@@ -24,6 +26,7 @@ class PrivacyPolicyPage extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
+            Sentry.captureException(snapshot.error);
             return Center(child: Text('Error loading policy'));
           } else {
             return Markdown(data: snapshot.data ?? '');
