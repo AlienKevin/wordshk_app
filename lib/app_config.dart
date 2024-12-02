88638fc0a3cdcc608aa9885f128513bf77189e87
Kevin Li <kevinli020508@gmail.com>
2024-12-01 17:22:29 -0800

    Added sync_rules.yml for constructing PowerSync per-user sync bucket

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,6 +1,6 @@
 // Enter your Supabase and PowerSync project details.
 class AppConfig {
-  static const String supabaseUrl = 'https://foo.supabase.co';
-  static const String supabaseAnonKey = 'foo';
-  static const String powersyncUrl = 'https://foo.powersync.journeyapps.com';
+  static const String supabaseUrl = 'https://xdctebxabvyultjepore.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkY3RlYnhhYnZ5dWx0amVwb3JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwODI5OTksImV4cCI6MjA0ODY1ODk5OX0.t5mrlGdmc2f00oY-xpGlqocyH6LnQ--2XRJOcBw8uU4';
+  static const String powersyncUrl = 'https://674cbf51937c44d039faa8ff.powersync.journeyapps.com';
 }
